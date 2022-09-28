a246443ac4de08159e1b418b775a727911287e11
Vitalii Baranetskyi <vitalik.baranetskiy@gmail.com>
2022-09-28 13:02:32 +0300

    Solved

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
     public void swap(int first, int second) {
+        int temp = first ^ second;
+        first = temp ^ first;
+        second = temp ^ second;
+        System.out.println(first);
+        System.out.println(second);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+        switch (month){
+            case 1,3,5,7,8,10,12 -> System.out.println(31);
+            case 4,6,9,11 -> System.out.println(30);
+            case 2 -> System.out.println(year % 100 == 0 && year % 400 != 0 ? 28 : year % 4 == 0 ? 29 : year <= 0 ? "invalid date" : 28);
+            default -> System.out.println("invalid date");
+        }
     }
 }
